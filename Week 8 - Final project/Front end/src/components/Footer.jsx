a842f6b0c91791abd31cb22301b983f84a3d1c41
arynyestos <arynyestos@gmail.com>
2023-08-18 13:58:25 +0200

    Remove comments

--- Week 8 - Final project/Front end/src/components/Footer.jsx
+++ Week 8 - Final project/Front end/src/components/Footer.jsx
@@ -3,23 +3,16 @@ import { Box, Flex, Image } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    // <Box width="100%" py={4} bg="gray.100">
-    //   <Flex justifyContent="center" alignItems="center" maxW="1200px" mx="auto">
-    //     <Image src="/logo-chainlink.png" alt="Chainlink Logo" height="60px" />
-    //     <Image src="/logo-ethereum.png" alt="Ethereum Logo" height="60px" />
-    //     <Image src="/logo-alchemy.png" alt="Alchemy Logo" height="60px" />
-    //   </Flex>
-    // </Box>
     <Box width="100%" py={2}>
   <Flex justifyContent="center" alignItems="center" maxW="1200px" mx="auto">
     <Flex alignItems="center" justifyContent="center" flex={4}>
-      <Image src="/logo-chainlink.png" alt="Chainlink Logo" height="70px" />
+      <Image src="/logo-chainlink.png" alt="Chainlink Logo" height="4em" />
     </Flex>
     <Flex alignItems="center" justifyContent="center" flex={1}>
-      <Image src="/logo-ethereum.png" alt="Ethereum Logo" height="70px" />
+      <Image src="/logo-ethereum.png" alt="Ethereum Logo" height="4em" />
     </Flex>
     <Flex alignItems="center" justifyContent="center" flex={4}>
-      <Image src="/logo-alchemy.png" alt="Alchemy Logo" height="70px" />
+      <Image src="/logo-alchemy.png" alt="Alchemy Logo" height="4em" />
     </Flex>
   </Flex>
 </Box>
