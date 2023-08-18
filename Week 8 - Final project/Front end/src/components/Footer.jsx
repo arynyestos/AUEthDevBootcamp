446c88ce7d0ad69a6baac5bb1ed10f002d8fcbe3
arynyestos <arynyestos@gmail.com>
2023-08-18 13:59:15 +0200

    Change icon size

--- Week 8 - Final project/Front end/src/components/Footer.jsx
+++ Week 8 - Final project/Front end/src/components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
     <Box width="100%" py={2}>
   <Flex justifyContent="center" alignItems="center" maxW="1200px" mx="auto">
     <Flex alignItems="center" justifyContent="center" flex={4}>
-      <Image src="/logo-chainlink.png" alt="Chainlink Logo" height="4em" />
+      <Image src="/logo-chainlink.png" alt="Chainlink Logo" height="4.5em" />
     </Flex>
     <Flex alignItems="center" justifyContent="center" flex={1}>
-      <Image src="/logo-ethereum.png" alt="Ethereum Logo" height="4em" />
+      <Image src="/logo-ethereum.png" alt="Ethereum Logo" height="4.5em" />
     </Flex>
     <Flex alignItems="center" justifyContent="center" flex={4}>
-      <Image src="/logo-alchemy.png" alt="Alchemy Logo" height="4em" />
+      <Image src="/logo-alchemy.png" alt="Alchemy Logo" height="4.5em" />
     </Flex>
   </Flex>
 </Box>
